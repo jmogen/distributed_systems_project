542ec0bdd04c39397c87a3eb359af56667117621
jmogen <jmogen@uwaterloo.ca>
2025-07-01 17:45:41 -0400

    cluster improvement

--- assignment_2/a2_starter/Task1.scala
+++ assignment_2/a2_starter/Task1.scala
@@ -3,10 +3,13 @@ import org.apache.spark.{SparkContext, SparkConf}
 // please don't change the object name
 object Task1 {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Task 1")
+    val conf = new SparkConf()
+      .setAppName("Task 1")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     val sc = new SparkContext(conf)
 
-    val textFile = sc.textFile(args(0))
+    val numPartitions = if (args.length > 2) args(2).toInt else sc.defaultParallelism
+    val textFile = sc.textFile(args(0), numPartitions)
 
     // Efficient map: no extra collections, no shuffles, no wide dependencies
     val output = textFile.map { line =>
@@ -15,7 +18,8 @@ object Task1 {
       // Only parse ratings that are non-empty, keep their indices
       var maxRating = Int.MinValue
       val ratings = new Array[Int](parts.length - 1)
-      for (i <- 1 until parts.length) {
+      var i = 1
+      while (i < parts.length) {
         if (parts(i).nonEmpty) {
           val rating = parts(i).trim.toInt
           ratings(i - 1) = rating
@@ -23,10 +27,21 @@ object Task1 {
         } else {
           ratings(i - 1) = Int.MinValue // Mark as missing
         }
+        i += 1
       }
       // Find all user indices with the max rating
-      val userIndices = for (i <- ratings.indices if ratings(i) == maxRating) yield (i + 1)
-      movie + "," + userIndices.mkString(",")
+      val userIndices = new StringBuilder
+      var first = true
+      i = 0
+      while (i < ratings.length) {
+        if (ratings(i) == maxRating) {
+          if (!first) userIndices.append(",")
+          userIndices.append(i + 1)
+          first = false
+        }
+        i += 1
+      }
+      movie + "," + userIndices.toString
     }
     
     output.saveAsTextFile(args(1))
