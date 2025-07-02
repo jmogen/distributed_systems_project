39d34da2d6f69c4c6609385de7bb79c115c96f80
jmogen <jmogen@uwaterloo.ca>
2025-07-01 20:14:06 -0400

    optimization

--- assignment_2/a2_starter/Task4.scala
+++ assignment_2/a2_starter/Task4.scala
@@ -8,37 +8,32 @@ object Task4 {
 
     val textFile = sc.textFile(args(0))
 
-    // Read all lines and split into (movie name, ratings array)
-    val lines = textFile.map(line => {
-      val parts = line.split(",").map(_.trim)
+    // Parse each line into (movie, ratings) pairs
+    val movies = textFile.map { line =>
+      val parts = line.split(",", -1).map(_.trim)
       val movie = parts(0)
       val ratings = parts.drop(1)
       (movie, ratings)
-    }).collect()
-
-    // Get all unique pairs of movies (sorted lexicographically)
-    val moviePairs = for {
-      i <- lines.indices
-      j <- (i + 1) until lines.length
-      movieA = lines(i)._1
-      movieB = lines(j)._1
-      if movieA < movieB // lexicographic order
-    } yield ((movieA, lines(i)._2), (movieB, lines(j)._2))
-
-    // For each pair, compute similarity
-    val results = moviePairs.map { case ((movieA, ratingsA), (movieB, ratingsB)) =>
-      val minLen = math.min(ratingsA.length, ratingsB.length)
-      var similarity = 0
-      for (k <- 0 until minLen) {
-        if (ratingsA(k).nonEmpty && ratingsB(k).nonEmpty && ratingsA(k) == ratingsB(k)) {
-          similarity += 1
-        }
-      }
-      s"$movieA,$movieB,$similarity"
     }
 
+    // Generate all movie pairs using cartesian product
+    val moviePairs = movies.cartesian(movies)
+      .filter { case ((movieA, _), (movieB, _)) => movieA < movieB } // lexicographic order
+      .map { case ((movieA, ratingsA), (movieB, ratingsB)) =>
+        // Compute similarity: count users who gave same rating to both movies
+        val minLen = math.min(ratingsA.length, ratingsB.length)
+        var similarity = 0
+        var k = 0
+        while (k < minLen) {
+          if (ratingsA(k).nonEmpty && ratingsB(k).nonEmpty && ratingsA(k) == ratingsB(k)) {
+            similarity += 1
+          }
+          k += 1
+        }
+        s"$movieA,$movieB,$similarity"
+      }
+
     // Output the results
-    val output = sc.parallelize(results)
-    output.saveAsTextFile(args(1))
+    moviePairs.saveAsTextFile(args(1))
   }
 }
